dc5823a2aa59901801c0ef5a34cb8807dd50ddca
Lucas Brunori <lucas.brunori@gmail.com>
2021-05-20 01:17:19 -0300

    Added basic authentication to the application

---
+++ src/main/java/com/w2m/cache/CustomBasicAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.w2m.cache;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.w2m.exception.APIErrorResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
+        response.addHeader("content-type", "application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        APIErrorResponse error = new APIErrorResponse("unauthenticated", "HTTP Status 401 - " + authException.getMessage());
+        writer.println(objectMapper.writeValueAsString(error));
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("heroes-api");
+        super.afterPropertiesSet();
+    }
+}
--- src/main/java/com/w2m/cache/SecurityConfig.java
+++ src/main/java/com/w2m/cache/SecurityConfig.java
@@ -1,4 +1,44 @@
 package com.w2m.cache;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomBasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin"))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(GET, "/api/heroes/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
